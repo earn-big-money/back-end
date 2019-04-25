36e5d4712c18e280a4537363b458e6568218a2b2
lihaoliang <2713014632@qq.com>
2019-04-25 23:55:05 +0800

    ignore sessions

--- earn-big-money/routes/users.js
+++ earn-big-money/routes/users.js
@@ -2,15 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('./../controller/DBController_Yukikaze');
 
-router.get('/', function(req, res, next) {
-	if(req.session.user) {
-		res.send(req.session.user);
-	}
-	else {
-		res.send('no cookie');
-	}
-});
-
 router.post('/create', function(req, res, next) {
 	db.insertUser({
 		"uid": req.body.id,
@@ -41,12 +32,12 @@ router.post('/login', function(req, res, next) {
 						if (resultFromDatabase[0] == undefined || 
 						req.body.password !== resultFromDatabase[0].upassword) {
 							res.status(400);
-							res.send("Incorrect username or password");
+							res.send({"msg" : "Incorrect username or password"});
 						}
 						else {
 							req.session.regenerate((err) => {
 								req.session.user = resultFromDatabase[0];
-								res.send("Success");
+								res.send({"msg" : "Success"});
 							});
 						}
 					});//回调函数，
@@ -54,8 +45,8 @@ router.post('/login', function(req, res, next) {
 
 router.get('/logout', function(req, res, next) {
 	req.session.destroy((err) => {
-		res.clearCookie("ebm");
-		res.send("Success");
+		res.clearCookie("EBMcookie");
+		res.send({"msg" : "Success"});
 	});
 });
 
