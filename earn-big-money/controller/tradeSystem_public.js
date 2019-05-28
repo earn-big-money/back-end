4b3947ea0ede85a68f5dee4fda3b18a443383d26
sherluck314 <13371350035@163.com>
2019-05-28 22:20:46 +0800

    查看余额

--- earn-big-money/controller/tradeSystem_public.js
+++ earn-big-money/controller/tradeSystem_public.js
@@ -3,9 +3,53 @@ var db = require('./DBController_public');
 var tradeSystem = function() {
 	this.version = "1.0.0";
 
-	// 用于创建交易
-	this.createTrade = function(args, callback) {
-		
+	// 用于查询余额
+	this.getBalance = function(req, res, next) {
+		if (req.session.user == null) {
+			res.redirect('localhost:8080/users/login');
+		}
+		else {
+			let strc = db.getSQLObject();
+			console.log(req.query.id)
+			strc["query"] = 'select';
+			strc["tables"] = "userInfo";
+			strc["data"] = {
+				"umoney": 0
+			};
+			strc["where"]["condition"] = [
+				"uname  = " + db.typeTransform(req.session.user.uname),
+				"uemail = " + db.typeTransform(req.session.user.uemail),
+				"uphone = " + db.typeTransform(req.session.user.uphone)
+			];
+			strc["where"]["type"] = "or";
+			db.ControlAPI_obj(strc, (resultFromDatabase)=>{
+				console.log(resultFromDatabase[0]); // 取下标为0即可
+				if (resultFromDatabase[0] == undefined) {
+					res.send({"msg":"fail to check the balance"})
+				}
+				else {
+					res.send({"balance":resultFromDatabase[0].umoney})
+				}
+			});//回调函数，
+		}
+	}
+
+	// 用于用户充值
+	this.topup = function(req, res, next) {
+		if (req.session.user == null) {
+			res.redirect('localhost:8080/users/login');
+		}
+		else {
+		}
+	}
+
+	// 用于转账
+	this.createTrade = function(req, res, next) {
+		if (req.session.user == null) {
+			res.redirect('localhost:8080/users/login');
+		}
+		else {
+		}
 	}
 	
 	// 用于取消交易
--- earn-big-money/routes/transactions.js
+++ earn-big-money/routes/transactions.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var tradeSystem = require('./../controller/tradeSystem_public');
 // 获取登录用户余额
 router.get('/', function(req, res, next) {
-	res.send({msg: 'balance'});
+	tradeSystem.getBalance(req, res, next);
 });
 
 // 登录用户充值
 router.post('/topup', function(req, res, next) {
-	res.send({msg: 'balance'});
+	tradeSystem.topup(req, res, next);
 });
 
 // 登录用户转账
 router.get('/transfer', function(req, res, next) {
-	res.send({msg: 'balance'});
+	tradeSystem.transfer(req, res, next);
 });
 
 module.exports = router;
