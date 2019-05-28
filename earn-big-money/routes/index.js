be72ba41b832143780b9c5cd92def8a89ce8c74c
sherluck314 <13371350035@163.com>
2019-05-28 10:51:31 +0800

    加入登录状态检测

--- earn-big-money/routes/index.js
+++ earn-big-money/routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.send({title: 'earn big money'});
+	if (req.session.user) {
+		res.send(req.session.user);
+	}
+	else {
+		res.send({title: 'earn big money'});
+	}
 });
 
 module.exports = router;
+1
