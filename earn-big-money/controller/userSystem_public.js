606c0098dae02843071b95f071b5f7e100ae3e74
sherluck314 <13371350035@163.com>
2019-06-01 19:55:37 +0800

    修改异步回调方式，进行全面测试

--- earn-big-money/controller/userSystem_public.js
+++ earn-big-money/controller/userSystem_public.js
@@ -1,4 +1,5 @@
 var db = require('./DBController_public');
+var utils = require('./Utils_public')
 
 var userSystem = function() {
 	
@@ -9,58 +10,57 @@ var userSystem = function() {
 	`
 	
 	// 用户注册
-	this.createUser = function(req, res, next) {
-		let strc = db.getSQLObject();
-		console.log(req.body)
-		strc["query"] = 'insert';
-		strc["tables"] = "userInfo";
-		strc["data"] = {
-			"uid": req.body.id,
-			"uname": req.body.username,
-			"upassword": req.body.password,
-			"uphone": req.body.phone,
-			"uemail": req.body.email,
-			"utype": req.body.status
-		};//传入一个结构体
-		db.ControlAPI_obj(strc, (resultFromDatabase)=>{
-			//console.log(resultFromDatabase); // 取下标为0即可
+	this.createUser = async function(req, res, next) {
+		try {
+			let strc = db.getSQLObject();
+			console.log(req.body)
+			strc["query"] = 'insert';
+			strc["tables"] = "userInfo";
+			strc["data"] = {
+				"uid": req.body.id,
+				"uname": req.body.username,
+				"upassword": req.body.password,
+				"uphone": req.body.phone,
+				"uemail": req.body.email,
+				"utype": req.body.status
+			};//传入一个结构体
+			let resultFromDatabase = await db.ControlAPI_obj_async(strc);
 			if (resultFromDatabase == null) {
-				res.status(400);
-				res.send({"msg" : "Invaild message"});
+				utils.sendError(res, 400, "Invaild message");
 			}
 			else {
 				res.send({
 					"msg" : "Success"
 				});
 			}
-		});//回调函数，	
+		}
+		catch(err) {
+			utils.sendError(res, 400, "Invaild message");
+		}
 	};
 	
 	// 用户登录
-	this.loginUser = function(req, res, next) {
+	this.loginUser = async function(req, res, next) {
 		if (req.session.user) {
-			res.status(400);
-			res.send({
-				"msg" : "[" + req.session.user.uid + "] already log in"
-			});
+			utils.sendError(res, 400, "[" + req.session.user.uid + "] already log in");
 		}
 		else {
-			let strc = db.getSQLObject();
-			strc["query"] = 'select';
-			strc["tables"] = "userInfo";
-			strc["data"] = {
-				"uid": 0
-			};
-			strc["where"]["condition"] = [
-				`(uid = ${db.typeTransform(req.body.id)} or
-				 uemail = ${db.typeTransform(req.body.id)} or
-				 uphone = ${db.typeTransform(req.body.id)})`,
-				"upassword = " + db.typeTransform(req.body.password)
-			];
-			db.ControlAPI_obj(strc, (resultFromDatabase)=>{
+			try {
+				let strc = db.getSQLObject();
+				strc["query"] = 'select';
+				strc["tables"] = "userInfo";
+				strc["data"] = {
+					"uid": 0
+				};
+				strc["where"]["condition"] = [
+					`(uid = ${db.typeTransform(req.body.id)} or
+					 uemail = ${db.typeTransform(req.body.id)} or
+					 uphone = ${db.typeTransform(req.body.id)})`,
+					"upassword = " + db.typeTransform(req.body.password)
+				];
+				let resultFromDatabase = await db.ControlAPI_obj_async(strc);
 				if (resultFromDatabase == null || resultFromDatabase.length == 0) {
-					res.status(400);
-					res.send({"msg" : "Incorrect id or password"});
+					utils.sendError(res, 400, "Incorrect username or password");
 				}
 				else {
 					req.session.regenerate((err) => {
@@ -70,7 +70,10 @@ var userSystem = function() {
 						});
 					});
 				}
-			});
+			}
+			catch(err) {
+				utils.sendError(res, 400, "Incorrect username or password");
+			}
 		}
 	};
 	
@@ -83,27 +86,29 @@ var userSystem = function() {
 	};
 	
 	// 用户查找，感觉查找的内容可以是任意一个用户，所以没必要做登陆检测
-	this.queryUser = function(req, res, next) {
-		let strc = db.getSQLObject();
-		strc["query"] = 'select';
-		strc["tables"] = "userInfo";
-		strc["data"] = {
-			"uid": 0,
-			"uname": 0,
-			"uphone": 0,
-			"uemail": 0,
-			"utype": 0,
-			"ucreatetime": 0
-		};
-		strc["where"]["condition"] = [
-			"uid    = " + db.typeTransform(req.params.account),
-			"uemail = " + db.typeTransform(req.params.account),
-			"uphone = " + db.typeTransform(req.params.account)
-		];
-		strc["where"]["type"] = "or";
-		db.ControlAPI_obj(strc, (resultFromDatabase)=>{
+	this.queryUser = async function(req, res, next) {
+		console.log(req.params)
+		try {
+			let strc = db.getSQLObject();
+			strc["query"] = 'select';
+			strc["tables"] = "userInfo";
+			strc["data"] = {
+				"uid": 0,
+				"uname": 0,
+				"uphone": 0,
+				"uemail": 0,
+				"utype": 0,
+				"ucreatetime": 0
+			};
+			strc["where"]["condition"] = [
+				"uid = " + db.typeTransform(req.params.account),
+				"uemail = " + db.typeTransform(req.params.account),
+				"uphone = " + db.typeTransform(req.params.account)
+			];
+			strc["where"]["type"] = "or";
+			let resultFromDatabase = await db.ControlAPI_obj_async(strc);
 			if (resultFromDatabase == null || resultFromDatabase.length == 0) {
-				res.send({"msg" : `User[${req.params.account}] does not exist`})
+				utils.sendError(res, 400,`User[${req.params.account}] does not exist`);
 			}
 			else {
 				res.send({
@@ -115,44 +120,50 @@ var userSystem = function() {
 					"createTime" : resultFromDatabase[0]["ucreatetime"]
 				});
 			}
-		});
+		}
+		catch(err) {
+			utils.sendError(res, 400, "Please login first.");
+		}
 	};
 
 	// 用户更新个人信息
-	this.updateUser = function(req, res, next) {
+	this.updateUser = async function(req, res, next) {
 		if (req.session.user.uid != req.params.account) {
-			res.status(400);
-			res.send({'msg':'Unauthorized operations.'});
+			utils.sendError(res, 400, 'Unauthorized operations.');
 			return;
 		}
-		let strc = db.getSQLObject();
-		strc["query"] = 'update';
-		strc["tables"] = "userInfo";
-		if (req.body.username != null) {
-			strc["data"]["uname"] = req.body.username;
-		}
-		if (req.body.phone != null) {
-			strc["data"]["uphone"] = req.body.phone;
-		}
-		if (req.body.email != null) {
-			strc["data"]["uemail"] = req.body.email;
-		}
-		if (req.body.status != null) {
-			strc["data"]["utype"] = req.body.status;
-		}
-		strc["where"]["condition"] = [
-			"uid  = " + db.typeTransform(req.session.user.uid)
-		];
-		db.ControlAPI_obj(strc, (resultFromDatabase) => {
+		try {
+			let strc = db.getSQLObject();
+			strc["query"] = 'update';
+			strc["tables"] = "userInfo";
+			if (req.body.username != null) {
+				strc["data"]["uname"] = req.body.username;
+			}
+			if (req.body.phone != null) {
+				strc["data"]["uphone"] = req.body.phone;
+			}
+			if (req.body.email != null) {
+				strc["data"]["uemail"] = req.body.email;
+			}
+			if (req.body.status != null) {
+				strc["data"]["utype"] = req.body.status;
+			}
+			strc["where"]["condition"] = [
+				"uid  = " + db.typeTransform(req.session.user.uid)
+			];
+			let resultFromDatabase = await db.ControlAPI_obj_async(strc);
 			console.log(resultFromDatabase);
 			if (resultFromDatabase == null) {
-				res.status(400);
-				res.send({"msg": "Failed in modification."});
+				utils.sendError(res, 400, "Failed in modification.");
 			}
 			else {
 				res.send({"msg" : "Success"})
 			}
-		});
+		}
+		catch(err) {
+			utils.sendError(res, 400, "Failed in modification.");
+		}
+		
 	};
 	
 	// 用户群组查找
