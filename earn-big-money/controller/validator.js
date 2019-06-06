212a9fff4940f684a3d89d5194fa5fbfef7986ad
ZTao-z <1124693098@qq.com>
2019-06-06 22:10:46 +0800

    fix bug in validator

--- earn-big-money/controller/validator.js
+++ earn-big-money/controller/validator.js
@@ -1,19 +1,22 @@
-var validator = {};
-// 账号
-validator.checkID = function(input){
-  return /^[a-zA-Z][0-9a-zA-Z_]{4,6}[0-9a-zA-Z_]$/.test(input) || !input;
-};
-validator.checkPassword = function(input){
-  return /^[a-zA-Z0-9_\-][0-9a-zA-Z_\-]{4,10}[0-9a-zA-Z_\-]$/.test(input) || !input;
+
+var validator = function(){
+	// 账号
+	this.checkID = function(input){
+	  return /^[a-zA-Z][0-9a-zA-Z_]{4,6}[0-9a-zA-Z_]$/.test(input) || !input;
+	};
+	this.checkPassword = function(input){
+	  return /^[a-zA-Z0-9_\-][0-9a-zA-Z_\-]{4,10}[0-9a-zA-Z_\-]$/.test(input) || !input;
+	};
+
+	// 手机
+	this.checkPhone = function(input){
+	  return /^[1-9][0-9]{9}[0-9]$/.test(input) || !input;
+	};
+	// 邮箱
+	this.checkEmail = function(input){
+	  return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(input) || !input;
+	};
 };
 
-// 手机
-validator.checkPhone = function(input){
-  return /^[1-9][0-9]{9}[0-9]$/.test(input) || !input;
-};
-// 邮箱
-validator.checkEmail = function(input){
-  return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(input) || !input;
-};
 
-module.export = validator;
+module.exports = new validator();
