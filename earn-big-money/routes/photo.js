3a84b4748a41a9ac2d261a5a8b19ed0bf53b7bce
lihaoliang <2713014632@qq.com>
2019-06-11 00:39:11 +0800

    修改路由与API文档匹配

--- earn-big-money/routes/photo.js
+++ earn-big-money/routes/photo.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var photo = require('./../controller/imageSystem_public');
 var utils = require('./../controller/Utils_public');
 
-router.post('/uploadUserPhoto', utils.loginCheck, photo.uploadUserPhoto);
-
-router.post('/uploadDutyPhoto', utils.loginCheck, photo.uploadDutyPhoto);
-
+// 获取用户头像
 router.get('/userPhoto/:id', photo.getUserPhoto);
-
+// 上传用户头像
+router.post('/uploadUserPhoto', utils.loginCheck, photo.uploadUserPhoto);
+// 获取任务图片
 router.get('/dutyPhoto/:id', photo.getDutyPhoto);
+// 上传任务图片
+router.post('/uploadDutyPhoto', utils.loginCheck, photo.uploadDutyPhoto);
 
 module.exports = router;
